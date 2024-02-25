ae73d721ba66c95291fea58f7884dacca973d505
samu63cs <98705451+samu63cs@users.noreply.github.com>
2024-02-25 15:39:32 -0700

    Add files via upload

---
+++ ndsgame/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ndsgame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBoxBuscar = new System.Windows.Forms.TextBox();
+            this.buttonDescargar = new System.Windows.Forms.Button();
+            this.dwFolder = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dwFolderobj = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(264, 205);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBoxBuscar
+            // 
+            this.textBoxBuscar.Location = new System.Drawing.Point(12, 234);
+            this.textBoxBuscar.Name = "textBoxBuscar";
+            this.textBoxBuscar.Size = new System.Drawing.Size(265, 20);
+            this.textBoxBuscar.TabIndex = 1;
+            this.textBoxBuscar.TextChanged += new System.EventHandler(this.textBoxBuscar_TextChangedAsync);
+            // 
+            // buttonDescargar
+            // 
+            this.buttonDescargar.Location = new System.Drawing.Point(12, 291);
+            this.buttonDescargar.Name = "buttonDescargar";
+            this.buttonDescargar.Size = new System.Drawing.Size(265, 29);
+            this.buttonDescargar.TabIndex = 2;
+            this.buttonDescargar.Text = "Download";
+            this.buttonDescargar.UseVisualStyleBackColor = true;
+            this.buttonDescargar.Click += new System.EventHandler(this.buttonDescargar_Click);
+            // 
+            // dwFolder
+            // 
+            this.dwFolder.Location = new System.Drawing.Point(124, 267);
+            this.dwFolder.Name = "dwFolder";
+            this.dwFolder.Size = new System.Drawing.Size(153, 20);
+            this.dwFolder.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Download Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 328);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dwFolder);
+            this.Controls.Add(this.buttonDescargar);
+            this.Controls.Add(this.textBoxBuscar);
+            this.Controls.Add(this.listView1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ndsgame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBoxBuscar;
+        private System.Windows.Forms.Button buttonDescargar;
+        private System.Windows.Forms.TextBox dwFolder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog dwFolderobj;
+    }
+}
+
---
+++ ndsgame/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Http;
+using SharpCompress.Archives;
+using SharpCompress.Archives.SevenZip;
+using fnt;
+using System.Net;
+using System.Diagnostics;
+
+namespace ndsgame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            
+            textBoxBuscar.Name = "txtBusqueda";
+
+            // Asignar nombre a un ListView
+            listView1.Name = "lstJuegos";
+
+            // Asignar nombre a un Button
+            buttonDescargar.Name = "btnDescargar";
+        }
+        static void Descomprimir7Zip(string archivo7zip, string directorioDestino)
+        {
+            using (var archivo = SevenZipArchive.Open(archivo7zip))
+            {
+                foreach (var entrada in archivo.Entries)
+                {
+                    if (!entrada.IsDirectory)
+                    {
+                        entrada.WriteToDirectory(directorioDestino, new SharpCompress.Common.ExtractionOptions()
+                        {
+                            ExtractFullPath = true,
+                            Overwrite = true
+                        });
+                    }
+                }
+            }
+        }
+            private void Form1_Load(object sender, EventArgs e)
+        {
+            // Agregar columnas al ListView
+            listView1.Columns.Add("Name", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("URL", -2, HorizontalAlignment.Left);
+
+            // Agregar más columnas según sea necesario
+
+            CargarDatos();
+
+
+
+            BuscarYMostrarResultados("");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dwFolderobj.ShowDialog();
+            if(dwFolderobj.ShowDialog() == DialogResult.OK)
+            {
+                dwFolder.Text = dwFolderobj.SelectedPath;
+            }
+        }
+
+        List<KeyValuePair<string, string>> juegos = new List<KeyValuePair<string, string>>();
+
+        // Cargar datos desde archivo
+        void CargarDatos()
+        {
+            var lineas = File.ReadAllLines("datos.txt");
+            foreach (var linea in lineas)
+            {
+                var partes = linea.Split('|');
+                if (partes.Length == 2)
+                {
+                    juegos.Add(new KeyValuePair<string, string>(partes[0], partes[1]));
+                }
+            }
+        }
+
+       
+        async Task BuscarYMostrarResultados(string busqueda)
+        {
+            var resultadosFiltrados = juegos
+                .Where(juego => juego.Value.IndexOf(busqueda, StringComparison.OrdinalIgnoreCase) >= 0)
+                .OrderBy(juego => juego.Value.IndexOf(busqueda, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            listView1.Items.Clear(); // Asumiendo que estás usando un ListView
+
+            foreach (var juego in resultadosFiltrados)
+            {
+                var item = new ListViewItem(juego.Value);
+                item.Tag = juego.Key; // Guarda la URL en el Tag para usarla después
+                listView1.Items.Add(item);
+            }
+            
+            
+        }
+        
+        
+        private void textBoxBuscar_TextChangedAsync(object sender, EventArgs e)
+        {
+             BuscarYMostrarResultados(textBoxBuscar.Text);
+            
+            
+        }
+
+        static async void DescargarArchivo(string url, string destino)
+        {
+            try
+            {
+               web.dwFile(url, destino);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al ejecutar curl: {ex.Message}");
+            }
+        }
+
+        async void DescargarArchivoSeleccionado()
+        {
+           
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var selectedItem = listView1.SelectedItems[0];
+                string url = (string)selectedItem.Tag;
+                string nombreArchivo = selectedItem.Text + ".7z"; // Ajusta según necesites
+               MessageBox.Show(url + " to: " + dwFolder.Text + @"\" + nombreArchivo);
+
+                DescargarArchivo(url, dwFolder.Text + @"\" + nombreArchivo);
+                Directory.CreateDirectory(dwFolder.Text + @"\nds");
+                    Descomprimir7Zip(dwFolder.Text + @"\" + nombreArchivo, dwFolder.Text + @"\nds");
+                MessageBox.Show("Done!");
+                   
+                
+            }
+        }
+
+        private void buttonDescargar_Click(object sender, EventArgs e)
+        {
+            Thread dwd = new Thread(DescargarArchivoSeleccionado);
+            dwd.Start();
+        }
+    }
+}
